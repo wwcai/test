d6583905faa983bd080a7e60248f777a051ff63d
dabaicai <dabaicai@qq.com>
2021-03-03 13:02:06 +0800

    123123

--- src/com/wwcai/domain/Student.java
+++ src/com/wwcai/domain/Student.java
@@ -1,6 +1,16 @@
 package com.wwcai.domain;
 
 public class Student {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     private String name;
     private String email;
 
